667d55207de23d41ca20d1edce9c538c81ec5fce
Erik Jiremalm <erji0002@student.hv.se>
2019-02-26 15:59:54 +0100

    dagens stor dåd är nu slutförda

---
+++ ETB310_TennantWebsite/Models/ServiceCaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ETB310_TennantWebsite.Models
+{
+    public class ServiceCaseViewModel
+    {
+        [Required(ErrorMessage = "Du måste skriva ett lägenhetsnumer.")]
+        [StringLength(4,ErrorMessage ="Lägenhetsnumret måste var fyra siffror.", MinimumLength =4)]
+        [RegularExpression(@"^\d{4}",ErrorMessage ="lägenhetsnumret måste var fyra siffror")]
+        public string FlatNr { get; set; } // optional 
+
+        public string Name { get; set; } // optional 
+
+        public string ContactEmail { get; set; } // obligatory 
+
+        public List<ServiceCasePostViewModel> Posts { get; set; }
+        public string NewPostMessage { get; set; }
+    }
+}
