1eec2cc82612eb3cb9e9f26e755173ac736d1707
Anders Andreen <anders.andreen@tele2.se>
2019-03-06 11:26:15 +0100

    Gjorde layout av webbplatsen

--- ETB310_TennantWebsite/Models/ServiceCaseViewModel.cs
+++ ETB310_TennantWebsite/Models/ServiceCaseViewModel.cs
@@ -8,11 +8,12 @@ namespace ETB310_TennantWebsite.Models
 {
     public class ServiceCaseViewModel
     {
-        [Required(ErrorMessage = "Du måste skriva ett lägenhetsnumer.")]
+        [Required(ErrorMessage = "Du måste ange ett lägenhetsnumer.")]
         [StringLength(4,ErrorMessage ="Lägenhetsnumret måste var fyra siffror.", MinimumLength =4)]
         [RegularExpression(@"^\d{4}",ErrorMessage ="lägenhetsnumret måste var fyra siffror")]
         public string FlatNr { get; set; } // optional 
 
+        [Required(ErrorMessage = "Du måste ange ett namn.")]
         [StringLength(30,ErrorMessage ="Namn måste vara minst 2 tecken och inte längre än 30 tecken", MinimumLength =2)]
         public string Name { get; set; } // optional 
 
